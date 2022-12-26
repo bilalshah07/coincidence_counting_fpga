2d0eb6e5534041c4d13422332af931d974f07406
bilalshah07 <100423002+bilalshah07@users.noreply.github.com>
2022-12-26 12:54:40 +0500

    Counting unit FPFA version 2
    This version ANDs the inputs first and then counts  detects them. It is therefore the AND of square pulses and is less sensitive to delay between channels. Would be good for QKD.

---
+++ Counting_unit_2/counting_unit_2/counting_unit_2.ip_user_files/ip/clk_wiz_0/clk_wiz_0_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+-- Date        : Tue Dec 20 16:16:18 2022
+-- Host        : PHY-113257LT running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               e:/Vivado_codes/Counting_unit_2/counting_unit_2/counting_unit_2.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.vhdl
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_out : out STD_LOGIC;
+    clk_in : in STD_LOGIC
+  );
+
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out,clk_in";
+begin
+end;
---
+++ Counting_unit_2/counting_unit_2/counting_unit_2.srcs/sources_1/new/main.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/20/2022 03:00:49 PM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
